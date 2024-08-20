c6ef1be29ff22a1334148ca7fde5b564cea18073
Mohamed Mansuri <117668345+Mansuri98@users.noreply.github.com>
2024-08-20 02:45:05 +0100

    WIP on main

---
+++ Assets/PrefrenceFrameUpdaterManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefrenceFrameUpdaterManager : MonoBehaviour
+{
+
+    public GameObject frameManger;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
